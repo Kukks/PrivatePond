45eaffd4d3fa97dbdac2d389011a345cadae4849
Kukks <evilkukka@gmail.com>
2021-01-05 14:29:50 +0100

    Initial commit

---
+++ PrivatePond/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrivatePond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

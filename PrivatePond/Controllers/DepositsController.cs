980162ad2bbdab9a7714a216b5d090908b025890
Kukks <evilkukka@gmail.com>
2021-01-20 11:00:41 +0100

    add deposit api

---
+++ PrivatePond/Controllers/DepositsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrivatePond.Controllers
+{
+    [Route("api/v1/deposits")]
+    public class DepositsController : ControllerBase
+    {
+        private readonly DepositService _depositService;
+
+        public DepositsController(DepositService depositService)
+        {
+            _depositService = depositService;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<DepositRequestData>> GetDepositRequest(string userId)
+        {
+            var result = await _depositService.GetOrGenerateDepositRequest(userId);
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return result;
+        }
+
+        [HttpGet("{userId}/history")]
+        public async Task<DepositRequestData> GetDepositRequestHistory(string userId)
+        {
+            return await _depositService.GetDepositRequestUserHistory(userId);
+        }
+    }
+}
